79028efbd44a3933435e17aeba74e6c457f279e4
Yurii Vynar <yurik.vinar37@gmail.com>
2023-04-02 20:29:38 +0300

    ade

--- controllers/auth/current.js
+++ controllers/auth/current.js
@@ -1,11 +1,20 @@
 const { User } = require("../../db/userModel");
+const jsonwebtoken = require("jsonwebtoken");
 
 const current = async (request, response) => {
-  const [, token] = req.headers.authorization.split(" ");
+  const [, token] = request.headers.authorization.split(" ");
   // const user = await User.findById(_id);
-  const user = await getCurrentUser(token);
+ // const user = await getCurrentUser(token);
   // console.log(user)
-  response.json({ token: user.token, user: user});
+  const user = jsonwebtoken.verify(token, process.env.JSONWEBTOKEN_SECRET);
+    const findedUser = await User.findByIdAndUpdate(user?._id).select([
+      "-password",
+      "-createdAt",
+      "-updatedAt",
+    ]);
+
+  if (!findedUser) throw new NotAuthorizedError("Not authorized");
+  response.json({ token: findedUser.token, user: findedUser });
 };
 
 module.exports = { current };
